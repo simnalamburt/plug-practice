26b0f21dbd53b4ba6d6bd5642ddbc66283ca5672
Hyeon Kim <simnalamburt@gmail.com>
2017-04-06 00:59:15 +0900

    Initialize project
    I used the command below:
    
        mix new plug-practice --app plug_practice

---
+++ lib/plug_practice.ex
@@ -0,0 +1,18 @@
+defmodule PlugPractice do
+  @moduledoc """
+  Documentation for PlugPractice.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugPractice.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_practice_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugPracticeTest do
+  use ExUnit.Case
+  doctest PlugPractice
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
